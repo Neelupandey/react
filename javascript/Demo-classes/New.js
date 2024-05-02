6daa33f3e4d1fd0f6b7d4436a28f0042cdd588b6
Neelupandey <98259407+Neelupandey@users.noreply.github.com>
2024-05-02 08:42:39 +0530

    modify

--- javascript/Demo-classes/New.js
+++ javascript/Demo-classes/New.js
@@ -1,8 +1,64 @@
 
 
-let x = 10;
-let y = 20;
-let z = x + y;
+// let x = 10;
+// let y = 20;
+// let z = x + y;
 
 
-console.log(z);
+// console.log(z);
+
+// let a = 100;
+// let b= 20;
+// let c = a / b;
+// ++a;
+// a++;
+// ++a;
+
+
+
+// console.log(a);
+
+// let i = 100;
+// let j = 10;
+// let k = i % j;
+// // i++;
+// // ++i;
+// // i++;
+// console.log(i+ j++);
+// console.log(i+ j++);
+
+// var num1=100;
+// ++num1;
+// num1--;
+// ++num1;
+// console.log(num1);
+
+// 10>30
+
+// var a=50;
+// var b=50;
+// //var c= a>=b;
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=b);
+
+//1
+console.log('----3---');
+var a=10;
+var b='ten';
+console.log(a==b);// fales
+console.log(a===b);// fales
+//2
+console.log('----2---');
+var a=100;
+var b='100';
+console.log(a==b);    // true               //type conversion will happen 
+console.log(a===b);   // fales             //no type conversion 
+//3
+console.log('----1---');
+var a=100;
+var b=100;
+console.log(a==b); // true
+console.log(a===b); // true
+
