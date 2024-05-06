2029d285d448ba3a427bd5d86296f249f0c24ab6
Neelupandey <98259407+Neelupandey@users.noreply.github.com>
2024-05-06 09:03:57 +0530

    upload new js file

---
+++ javascript/Demo-classes/if.js
@@ -0,0 +1,74 @@
+
+// var num1= prompt("Enter a number");
+// if(num1 %2==0){
+//     console.log("even no.");
+// }
+  
+// var num1= prompt("Enter a number");
+// if(num1 % 2===0){
+//     console.log(num1+" is even no.");
+// }
+// else{
+//     console.log(num1+"is odd no.");
+// }
+
+
+
+// var age = 21;
+// if (age >= 18 && age <  65) {
+//     console.log("eligible for policy");
+//   }
+//   else{
+//     console.log("not eligible for policy");
+//   }
+
+// var  price= 300;
+// var discount = 0;
+//   if (price < 100) {
+//     discount=0;
+//     //console.log("discount is 10 rs.")
+//   } else if (price< 200 && price>= 100) {
+//     discount= 15
+//     //console.log("discount is 15 rs. ")
+//   } else {
+//     discount= 50;
+//   }
+//   console.log("discount is "+ discount)
+
+
+var  num1 = 20;
+
+  if(num1 >= 6 && num1<= 12) {
+    console.log("good morning");
+  } else if(num1 > 12 && num1 <=18) {
+    console.log("good afternoon ");
+  } 
+  else{
+    console.log("good evening ");
+  }
+
+
+var greet ="ga";
+switch(greet){
+    case"gm":{
+        console.log("good morning")
+        bresk;
+
+    }
+    case"ga":{
+        console.log("good afternoon")
+        bresk;
+        
+    }
+    case"ge":{
+        console.log("good enving")
+        bresk;
+        
+    }
+    case"gm":{
+        console.log("good morning")
+        bresk;
+        
+    }
+}
+ 
