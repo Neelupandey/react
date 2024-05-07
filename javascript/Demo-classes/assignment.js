947173b66be39082cdc105eaec1cdc6833a3ccdf
Neelupandey <98259407+Neelupandey@users.noreply.github.com>
2024-05-07 23:02:47 +0530

    add new file

---
+++ javascript/Demo-classes/assignment.js
@@ -0,0 +1,52 @@
+//1.  Check whether the given number is divisible by 3 and 5 or not.
+
+// var i;
+// for(i=1;i<=100;i++){
+//   if(i%3==0 && i%5==0){
+//     console.log(i)
+//   }
+// }
+
+//------------------------------------------------------------------
+
+//2.  Print 5's Table on console
+
+// for ( var i = 1; i <= 10; i++) {
+//      console.log(i * 5);
+//     }
+
+
+//-------------------------------------------------------------------
+
+//3.  print the number between 5 to 100 which is divisible by 2 and 5.
+
+
+// for (var i = 5; i <= 100; i++) { 
+
+// 	if (i % 2 == 0 && i % 5 ==0) { 
+// 		console.log(i); 
+// 	} 
+// }
+
+
+//-----------------------------------------------------------------
+
+//4.  Find the Factorial of given Number.     Input : 5     Output : 120
+
+for (var i = 5; i <= 120; i++) { 
+
+	if (i * 2 == 0 || i *3 ==0 || i *4 ==0 || i *5 ==0 ) { 
+		console.log(i); 
+	} 
+}
+
+
+
+//----------------------------------------------------------------
+
+//5.  Print a String in reverse Order.  Input : "Hello"      Output : "olleH";
+
+// let name = "Hello";
+// for (let i = 1; i <= name; i--) {
+//    console.log(i);
+//   }
