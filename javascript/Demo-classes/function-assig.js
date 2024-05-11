40dabd18cef2f815da1fba3262751373df594a1f
Neelupandey <98259407+Neelupandey@users.noreply.github.com>
2024-05-11 23:01:40 +0530

    upload new file

---
+++ javascript/Demo-classes/function-assig.js
@@ -0,0 +1,66 @@
+//1.write a function that takes one number as input check whether number is positive or not
+
+// function NegPost(n) {
+//     if (n > 0) {
+//         console.log("Positive number");
+//     } else if (n < 0) {
+//         console.log("Negative number");
+//     } else {
+//         console.log("Zero");
+//     }
+// }
+// NegPost(-21); // Negative number
+// NegPost(30); // Positive number
+// NegPost(0); // Zero
+
+//-------------------------------------------------------------------------------------
+
+//2.Write a function which will take three parameters.find greatest number among three numbers. and print on console
+
+// function findLargest(num1, num2, num3) {
+// 	if (num1 >= num2 && num1 >= num3) {
+// 		return num1;
+// 	} else if (num2 >= num1 && num2 >= num3) {
+// 		return num2;
+// 	} else {
+// 		return num3;
+// 	}
+// }
+
+// // Example usage
+// const largestNumber = findLargest(101,50,231);
+// console.log("Largest number:", largestNumber);
+
+
+//-------------------------------------------------------------------
+
+//3.Write a function to take any number as input and find the factorial of given number.
+
+
+// function factorial(n){
+//     let answer = 1;
+//     if (n == 0 || n == 1){
+//       return answer;
+//     }
+//     else if(n > 1){
+//       for(var i = n; i >= 1; i--){
+//         answer = answer * i;
+//       }
+//       return answer;
+//     }
+//     else{
+//       return "number has to be positive."
+//     }  
+//   }
+//   let n = 7;
+//   answer = factorial(n)
+//   console.log("Factorial of " + n + " : " + answer);
+  
+
+//----------------------------------------------------------------------------
+
+//4.Write a function  that takes two strings as inputs print "true" if they are anagrams of each other.
+  
+
+
+
