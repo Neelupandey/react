4dbb9e9e9b01d89e8051f6fad9ce3eced0625534
Neelupandey <98259407+Neelupandey@users.noreply.github.com>
2024-05-26 09:07:12 +0530

    add new file

---
+++ javascript/Demo-classes/news.js
@@ -0,0 +1,11 @@
+//Write a function called add that takes two numbers as parameters and returns their sum.
+
+function add(a,b){
+    var sum = a+b;
+    return sum;
+    
+}
+var sum = add(100,200);
+console.log(sum);
+
+//Write a function called multiply that takes two numbers as parameters and returns their product.
