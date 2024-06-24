0da09080d3f6371dbf55e3deeb924f27eb140af0
Neelupandey <98259407+Neelupandey@users.noreply.github.com>
2024-06-24 08:54:54 +0530

    add new file

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,6 +1,8 @@
 //import logo from './logo.svg';
 // import images from "./images.jpeg"
  import './App.css';
+ import { Counter } from './counter/counter';
+ import { Form } from './form/form';
 
 // function App() {
 //   return (
@@ -26,57 +28,15 @@
 
 function App(){
   return(
-    < >
       <div className="Firstclass">
-      Hello
-       <div> <form class="row g-3">
-  <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4"/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4"/>
-  </div>
-  <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-  </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity"/>
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip"/>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
-</form></div>
+      
+       
+<p>This is react application.</p>
+        <Counter name="Rohit" greeting="Hello" fullName="Sharma"></Counter>
+        <Counter name="Virat" greeting="Hi"></Counter>
+        <Form></Form>
       </div>
-    </>
-    
-  )
+   );
 }
 
 export default App;
